0bca7a99f3e3183ca92739ef176bc9ac5b295b0a
IAmTrial <IAmRealTrial@gmail.com>
2017-03-28 00:10:47 -0700

    Port over fixes from 00.03.07.

--- BH/D2Intercepts.cpp
+++ BH/D2Intercepts.cpp
@@ -225,19 +225,30 @@ SkipChat:
 	}
 }
 
-void PositionMenuTab_Interception(void) {
+void __declspec(naked) PositionMenuTab_Interception(void) {
 	__asm
 	{
+		; Check that the mode is set to HD, otherwise run 640 mode code
 		cmp eax, 03
-		jne Mode640
-		mov dword ptr ds:[0x6FAB0000+0x11D354], 352
-		mov dword ptr ds:[0x6FAB0000+0x11D358], -110
-		jmp RepositionEnd
 
-Mode640:
-		mov dword ptr ds:[0x6FAB0000+0x11D354], 0
-		mov dword ptr ds:[0x6FAB0000+0x11D358], 0
+		pushad
+		mov eax, p_D2CLIENT_TabXOffset
+		jne PositionMenuTab_Mode640
 
-RepositionEnd:
+		; HD positions
+		; Position X offset
+		mov dword ptr ds:[eax], 352
+		; Position Y offset
+		mov dword ptr ds:[eax+4], -110
+
+		jmp PositionMenuTab_RepositionEnd
+
+PositionMenuTab_Mode640:
+		mov dword ptr ds:[eax], 0
+		mov dword ptr ds:[eax+4], 0
+
+PositionMenuTab_RepositionEnd:
+		popad
+		ret
 	}
 }
