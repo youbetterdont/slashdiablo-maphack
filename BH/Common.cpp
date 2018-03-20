b15d119f7640f0a53cc75645ac67279e91760b3d
IAmTrial <IAmRealTrial@gmail.com>
2018-03-19 23:42:50 -0700

    replace GetColorCode return type with wstring
    returning a static wchar_t is not so great when the value of the function needs to be saved

--- BH/Common.cpp
+++ BH/Common.cpp
@@ -1,3 +1,48 @@
+/**
+*
+* Common.cpp
+* BH: Copyright 2011 (C) McGod
+* SlashDiablo Maphack: Copyright (C) SlashDiablo Community
+*
+*  This file is part of SlashDiablo Maphack.
+*
+*  SlashDiablo Maphack is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as published
+*  by the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* This file incorporates work covered by the following copyright and
+* permission notice:
+*
+*   ==========================================================
+*   D2Ex2
+*   https://github.com/lolet/D2Ex2
+*   ==========================================================
+*   Copyright (c) 2011-2014 Bartosz Jankowski
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*   ==========================================================
+*
+*/
+
 #include <string>
 #include <vector>
 #include <Windows.h>
@@ -43,13 +88,12 @@ char* UnicodeToAnsi(const wchar_t* str)
 	return buf;
 }
 
-wchar_t* GetColorCode(int ColNo)
+std::wstring GetColorCode(int ColNo)
 {
-	static wchar_t Result[4];
 	wchar_t* pCol = D2LANG_GetLocaleText(3994);
-	if (!pCol) return L"";
-	swprintf_s(Result, 4, L"%s%c", pCol, (char)(ColNo + '0'));
-	return Result;
+	wostringstream Result;
+	Result << pCol << (wchar_t) (ColNo + '0');
+	return Result.str();
 }
 
 std::string Trim(std::string source) {
